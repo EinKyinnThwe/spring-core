a245ae5aa027a9930b4cdf942650d77835429e4d
EKThwe <einkyinnthwe@gmail.com>
2023-03-02 15:32:30 +0630

    parser

---
+++ spel-demo/src/main/java/com/demo/Main3.java
@@ -0,0 +1,28 @@
+package com.demo;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.expression.BeanFactoryResolver;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+import org.springframework.expression.spel.support.StandardEvaluationContext;
+
+public class Main3 {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        StandardEvaluationContext evaluationContext =
+                new StandardEvaluationContext();
+        evaluationContext.setBeanResolver(new BeanFactoryResolver(context));
+
+        SpelExpressionParser parser =
+                new SpelExpressionParser();
+
+        System.out.println(
+                parser.parseExpression("@springBean1.name")
+                        .getValue(evaluationContext).toString().toUpperCase()
+        );
+        System.out.println(
+                parser.parseExpression("@springBean1.getStreetName()")
+                        .getValue(evaluationContext)
+        );
+    }
+}
--- spel-demo/src/main/java/com/demo/beans/SpringBean1.java
+++ spel-demo/src/main/java/com/demo/beans/SpringBean1.java
@@ -3,10 +3,13 @@ package com.demo.beans;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+import java.util.Map;
+
 @Component
 public class SpringBean1 {
     @Value("${app.name}")
-    private String name;
+    public String name;
 
     @Value("#{ ${invoice.amount} * 0.2 }")
     private double invoiceDiscount;
@@ -15,11 +18,23 @@ public class SpringBean1 {
     private String streetName;
     @Value("#{bean2.hobby}")
     private String hobbyName;
+    @Value("${app.fruits}")
+    private List<String> fruits;
+    @Value("#{${app.map}}")
+    private Map<String,Integer> map;
+
+    public String getStreetName() {
+        return streetName;
+    }
 
     public void print() {
         System.out.println("Name:" + name);
         System.out.println("DiscountInvoice:" + invoiceDiscount);
         System.out.println("StreetName:" + streetName);
         System.out.println("Hobby Name:" + hobbyName);
+        fruits.forEach(s -> {
+            System.out.println(s.toUpperCase());
+        });
+        System.out.println(map);
     }
 }
